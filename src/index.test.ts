09984b3f8fc3710cd90743bcd364f02359cdc971
Colin <96976705+skrambl@users.noreply.github.com>
2024-08-06 10:20:04 +0000

    asd

--- src/index.test.ts
+++
@@ -1,6 +0,0 @@
-test('console.log the text "Hello world!"', () => {
-  console.log = jest.fn();
-  require('./index');
-  expect(console.log).toBeCalledTimes(1);
-  expect(console.log).toHaveBeenCalledWith('Hello world!');
-});
